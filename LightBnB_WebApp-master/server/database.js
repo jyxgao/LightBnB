950e9b9362895d1dcf21d1c35993fb169091fa1c
Jenny Gao <gao.jenny9@gmail.com>
2020-08-20 22:15:00 +0000

    add add Property to database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -115,7 +115,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, AVG(property_reviews.rating) AS average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   if (options.city) {
@@ -138,7 +138,6 @@ const getAllProperties = function(options, limit = 10) {
     queryString += ` AND cost_per_night < $${queryParams.length}`;
   }
   
-
   queryString += ` GROUP BY properties.id`;
 
   if (options.minimum_rating) {
@@ -167,9 +166,46 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id, 
+    property.title, 
+    property.description, 
+    property.thumbnail_photo_url, 
+    property.cover_photo_url, 
+    property.cost_per_night, 
+    property.parking_spaces,
+    property.number_of_bathrooms, 
+    property.number_of_bedrooms, 
+    property.country, 
+    property.street, 
+    property.city, 
+    property.province, 
+    property.post_code
+  ];
+  const queryString = `
+  INSERT INTO properties (
+    owner_id, 
+    title, 
+    description, 
+    thumbnail_photo_url, 
+    cover_photo_url, 
+    cost_per_night, 
+    parking_spaces, 
+    number_of_bathrooms, 
+    number_of_bedrooms, 
+    country, 
+    street, 
+    city, 
+    province, 
+    post_code
+  )
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *
+  ;`;
+
+  console.log(queryString, queryParams)
+  return pool.query(queryString, queryParams)
+  .then(res => res.rows);
+
 }
 exports.addProperty = addProperty;
